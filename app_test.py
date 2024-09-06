034e4fa87c1910c6129981c09a15fe5529e98f6c
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-09-06 17:23:32 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -3,7 +3,13 @@ import pandas as pd
 import numpy as np
 import streamlit.components.v1 as components
 
-#st.sidebar.title('zeee')
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
 
 
 st.set_page_config(layout="wide")
