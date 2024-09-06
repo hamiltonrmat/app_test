816458de536e1f2972acd0cbf9b2cd55ef4a2b05
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-09-06 14:21:08 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -6,6 +6,10 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 st.title('SkillQuest Maths')
 
-urll = "https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload"
+url = "https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload"
+components.iframe(url, height=900)
+
+urll = "https://docs.google.com/presentation/d/1s4FcHclfo5pW9HyvBKQSM1nVPaMlNJEgfWjBeceiYsU/pub?start=false&loop=false&delayms=3000&slide=id.p"
+
 components.iframe(urll, height=900)
 
