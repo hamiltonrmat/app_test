f8c1bb957941be90c2bd153f39ada17b09347f16
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-09-12 14:39:11 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -9,6 +9,7 @@ st.title('SkillQuest Maths')
 
 
 st.sidebar.markdown("# Main page 🎈")
+st.sidebar.write("ffffdf")
 
 url = "https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload"
 components.iframe(url, height=900)
