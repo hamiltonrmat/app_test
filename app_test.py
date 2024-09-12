d54e05825da85bfcd02cd2ce0971aeea371f55ef
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-09-12 16:13:45 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -29,7 +29,7 @@ option = st.selectbox(
 
 df = data[data['nomCompetence'] == option][['Responsable', 'nbXP', 'disponible', 'obligatoire', 'DescriptionCompetence']]
 df[['Responsable', 'nbXP', 'disponible', 'obligatoire']]
-df.DescriptionCompetence
+df.DescriptionCompetence[0]
 
 
 
