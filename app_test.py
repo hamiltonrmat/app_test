0b71e181bcc64cdc252b0313cd2913b46f1da619
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-09-12 14:48:00 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -7,8 +7,8 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 st.title('SkillQuest Maths')
 
-
-st.sidebar.header("maths.unilasalle.fr")
+st.sidebar.button("maths.unilasalle.fr")
+st.page_link("https://maths.unilasalle.fr/", label="HubMaths UniLaSalle", icon="🌎")
 st.sidebar.write("Explorez l'arbre de compétences mathématiques UniLaSalle")
 
 url = "https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload"
