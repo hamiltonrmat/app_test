9eb163f308efc8825f6f0cdcdd93726fef4bccde
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-09-12 16:25:57 +0200

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -9,6 +9,8 @@ st.set_page_config(layout="wide")
 st.title('SkillQuest Maths')
 
 st.sidebar.page_link("https://maths.unilasalle.fr", label="HubMaths UniLaSalle", icon="🌎")
+st.sidebar.page_link("https://moodle-beauvais.unilasalle.fr/course/view.php?id=1434", label="Moodle UniLaSalle SkillQuest", icon="🖥️")
+
 st.sidebar.write("Explorez l'arbre de compétences mathématiques UniLaSalle")
 
 url = "https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload"
