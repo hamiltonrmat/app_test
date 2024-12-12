bc1e54365ebba62d281b37e3aadf35e212d38a6f
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-12-12 13:53:50 +0100

    Update 🏠_Homepage.py

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -19,7 +19,7 @@ st.sidebar.page_link("https://moodle-beauvais.unilasalle.fr/course/view.php?id=1
 
 st.page_link("🏠_Homepage.py", label="Home", icon="🏠")
 st.page_link("pages/Informations personnelles.py", label="Informations personnelles", icon="2️⃣")
-st.page_link("pages/Détail par compétence.py", label="Détail par compétence", icon="1️⃣")
+st.page_link("pages/1️⃣_Détail par compétence.py", label="Détail par compétence", icon="1️⃣")
 st.page_link("pages/Situation Générale.py", label="Situation Générale", icon="3️⃣")
 st.page_link("pages/📄_Liste de Compétences.py", label="Situation Générale", icon="📄")
 st.page_link("https://maths.unilasalle.fr", label="Hub Maths UniLaSalle", icon="🖥️")
