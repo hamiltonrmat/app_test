810bd95b2bcabcdd2fa390671f022a37d0018253
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-12-13 09:58:38 +0100

    Update 📄_Liste de Compétences.py

--- pages/📄_Liste de Compétences.py
+++ pages/📄_Liste de Compétences.py
@@ -11,7 +11,7 @@ domaines = data.nomDomaine.unique()
 
 st.header("Liste de compétences")
 option = st.selectbox(
-    "Sélectioner un domaine:",domaines)
+    "Sélectioner un domaine:",domaines, placeholder="Select contact method...")
 comp_domaine = data[data['nomDomaine'] == option]
 compet = list(comp_domaine.nomCompetence)
 
