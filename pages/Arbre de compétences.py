828995a2dedca8fbd340ffb710ea145723131c59
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-12-13 11:16:27 +0100

    Update Arbre de compétences.py

--- pages/Arbre de compétences.py
+++ pages/Arbre de compétences.py
@@ -6,4 +6,4 @@ st.header('Arbre de compétences mathématiques')
 
 
 
-components.iframe("https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload", height=700, width=1000)
+components.iframe("https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=mathskillsq.drawio.html#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1JckKNze4ue0CTv6WzvzFtlcs34MNmWiI%26export%3Ddownload", height=768, width=1366)
