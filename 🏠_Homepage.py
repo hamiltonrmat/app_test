b78df1a974a56b2a8b94d0486917cac251799c7b
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-12-13 10:06:09 +0100

    Update 🏠_Homepage.py

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -6,27 +6,39 @@ import openpyxl
 
 
 st.set_page_config(layout="wide")
-st.title('SkillQuest Maths')
 
-st.image("media.png", width=300)
+col1, col2, col3 = st.columns(3)
 
-st.sidebar.header("Bienvenue !")
+with col1:
 
-st.sidebar.write("Explorez les compétences mathématiques de SkillQuest et votre situation dans l'aventure")
+with col2:
+  st.title('SkillQuest Maths')
+  
+  st.image("media.png", width=300)
+  
+  st.sidebar.header("Bienvenue !")
+  
+  st.sidebar.write("Explorez les compétences mathématiques de SkillQuest et votre situation dans l'aventure")
+  
+  
+  st.sidebar.page_link("https://maths.unilasalle.fr", label="HubMaths UniLaSalle", icon="🌎")
+  st.sidebar.page_link("https://moodle-beauvais.unilasalle.fr/course/view.php?id=1434", label="Moodle UniLaSalle SkillQuest", icon="🖥️")
+  
+  
+  st.page_link("🏠_Homepage.py", label="Home", icon="🏠")
+  st.page_link("pages/Détail par compétence.py", label="Détail par compétence", icon="1️⃣")
+  st.page_link("pages/Informations personnelles.py", label="Informations personnelles", icon="2️⃣")
+  st.page_link("pages/Situation Générale.py", label="Situation Générale", icon="3️⃣")
+  st.page_link("pages/📄_Liste de Compétences.py", label="Liste de compétences", icon="📄")
+  st.page_link("pages/Arbre de compétences.py", label="Arbre de compétences", icon="🌳")
+  st.page_link("https://maths.unilasalle.fr", label="Hub Maths UniLaSalle", icon="🖥️")
+  st.page_link("https://moodle-beauvais.unilasalle.fr/course/view.php?id=1434", label="Moodle SkillQuest", icon="📚")
+
+with col3:
 
 
-st.sidebar.page_link("https://maths.unilasalle.fr", label="HubMaths UniLaSalle", icon="🌎")
-st.sidebar.page_link("https://moodle-beauvais.unilasalle.fr/course/view.php?id=1434", label="Moodle UniLaSalle SkillQuest", icon="🖥️")
 
 
-st.page_link("🏠_Homepage.py", label="Home", icon="🏠")
-st.page_link("pages/Détail par compétence.py", label="Détail par compétence", icon="1️⃣")
-st.page_link("pages/Informations personnelles.py", label="Informations personnelles", icon="2️⃣")
-st.page_link("pages/Situation Générale.py", label="Situation Générale", icon="3️⃣")
-st.page_link("pages/📄_Liste de Compétences.py", label="Liste de compétences", icon="📄")
-st.page_link("pages/Arbre de compétences.py", label="Arbre de compétences", icon="🌳")
-st.page_link("https://maths.unilasalle.fr", label="Hub Maths UniLaSalle", icon="🖥️")
-st.page_link("https://moodle-beauvais.unilasalle.fr/course/view.php?id=1434", label="Moodle SkillQuest", icon="📚")
 
 
 
