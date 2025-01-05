584c335371a3fdabc10fe3a794d47e31734d7003
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2025-01-05 16:45:42 +0100

    Update Informations personnelles.py

--- pages/Informations personnelles.py
+++ pages/Informations personnelles.py
@@ -417,7 +417,7 @@ tab_gen['nb_argent'] = qtd_argent
 
 st.title('Informations personelles')
 
-st.write("Dernière mise à jour: 17/12/2024") 
+st.write("Dernière mise à jour: 06/01/2025") 
 
 cles = pd.DataFrame(liste_gen[['clé', 'mail']])
 cles = liste_gen.dropna()
